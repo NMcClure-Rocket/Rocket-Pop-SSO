400bfb5322f8a406d5efb7fda52b21fe9f8e4b4e
RedFrederick <noone@nowhere.com>
2025-12-08 14:40:54 -0500

    Added initial version of password hashing function.

--- src/main/java/com/example/rocketpop/repository/UserDatabase.java
+++ src/main/java/com/example/rocketpop/repository/UserDatabase.java
@@ -1,13 +1,23 @@
 package com.example.rocketpop.repository;
 
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+
+import javax.crypto.Cipher;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,6 +30,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserDatabase implements Database {
     public static final Logger logger = LoggerFactory.getLogger(UserDatabase.class);
+
+    @Value("${private.key}")
+    private String privateKeyString;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -195,5 +208,54 @@ public class UserDatabase implements Database {
         Object[] args = {username};
         List<User> count = jdbcTemplate.query(GETUSERQUERY, args, new UserMapper());
         return count.size() > 0;
+    }
+
+    /**
+     * Decrypt username and password sent by user, then generate and return a salted hash of the password.
+     */
+    public void hashPassword(String ciphertext) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        String plaintext = null;
+        PrivateKey privateKey = null;
+
+        try {
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            byte[] privateKeyByte = decoder.decode(privateKeyString);
+            privateKey = factory.generatePrivate(new PKCS8EncodedKeySpec(privateKeyByte));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        // Decrypt ciphertext
+        try {
+            byte[] cipherBytes = decoder.decode(ciphertext);
+            Cipher decryptCipher = Cipher.getInstance("RSA");
+            decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptBytes = decryptCipher.doFinal(cipherBytes);
+            plaintext = new String(decryptBytes, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        // Isolate password
+        String[] splitPlaintext = plaintext.split(",");
+        String username = splitPlaintext[0];
+        String rawPassword = splitPlaintext[1];
+
+        // Hash password
+        SecureRandom rng = new SecureRandom();
+        byte[] salt = new byte[16];
+        byte[] hashedPassword = null;
+        try{
+            rng.nextBytes(salt);
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt);
+            hashedPassword = md.digest(rawPassword.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        // TODO: Call DB
+
     }
 }
